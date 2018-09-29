56f89e8d45e21061f563f6fa4bc14c98dc7e1fe6
Ryan Durham <ryanrdurham@gmail.com>
2018-09-28 23:02:04 -0400

    setup to begin automation

--- src/VirtruAutomationTest.java
+++ src/VirtruAutomationTest.java
@@ -1,2 +1,14 @@
-public class VirtruAutomationTest {
-}
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class VirtruAutomationTest
+    {
+        public static void main(String[] args)
+        {
+            System.setProperty("webdriver.chrome.driver", "../Virtru-Automation-Test/Selenium/chromedriver.exe");
+            ChromeDriver driver = new ChromeDriver();
+            driver.get("http://www.google.com");
+        }
+    }
